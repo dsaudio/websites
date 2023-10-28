498eca4e105460c05ff69d68e336497a88115ea7
kjxbyz <kjxbyz@163.com>
2023-10-28 14:07:13 +0800

    fix: update google-analytics

--- components/shared/google-analytics.tsx
+++ components/shared/google-analytics.tsx
@@ -4,6 +4,8 @@ import Script from "next/script";
 const GoogleAnalytics = () => {
   const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ID;
 
+  if (!GA_TRACKING_ID) return null;
+
   return (
     <>
       <Script
