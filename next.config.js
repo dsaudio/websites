c189947c893f2a52b9140499665ef080a77c600e
kjxbyz <kjxbyz@163.com>
2024-01-27 18:50:33 +0800

    fix: update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,10 +10,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "visitor-badge.laobi.icu",
-      "vercel.com",
+    remotePatterns: [
+      { hostname: "lh3.googleusercontent.com" },
+      { hostname: "visitor-badge.laobi.icu" },
     ],
     unoptimized: true,
   },
