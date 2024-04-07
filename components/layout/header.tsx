b263a9aa20eaf5bea330a32ddfeed09f3fb34621
kjxbyz <kjxbyz888@outlook.com>
2024-04-07 20:01:03 +0800

    fix: update github links

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -50,7 +50,7 @@ export default function Header(props: LngProps) {
             <li className="h-8 w-8 sm:h-9 sm:w-9">
               <div className="relative inline-block text-left">
                 <Link
-                  href="https://github.com/dsaudio/dsaudio.github.io"
+                  href="https://github.com/alisoft/dsaudio"
                   target="_blank"
                   className="flex h-8 w-8 items-center justify-center overflow-hidden rounded-full transition-all duration-75 focus:outline-none active:scale-95 sm:h-9 sm:w-9"
                 >
