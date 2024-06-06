126f3b4e3ecbf3463f1ab702c5d714166e916397
kjxbyz <kjxbyz888@outlook.com>
2024-06-06 21:14:04 +0800

    fix: update lints

--- app/[lng]/not-found.tsx
+++ app/[lng]/not-found.tsx
@@ -15,11 +15,11 @@ export default function NotFound() {
         <h1 className="mt-4 text-3xl font-bold tracking-tight dark:text-gray-300 sm:text-5xl">
           Page not found
         </h1>
-        <p className="mt-4 text-base dark:text-gray-300  sm:mt-6">
+        <p className="mt-4 text-base dark:text-gray-300 sm:mt-6">
           Sorry, we couldn’t find the page you’re looking for.
         </p>
         <div className="mt-10 flex justify-center dark:text-gray-300">
-          <Link href="/" className="text-sm font-semibold leading-7 ">
+          <Link href="/" className="text-sm font-semibold leading-7">
             <span aria-hidden="true">&larr;</span> Back to home
           </Link>
         </div>
